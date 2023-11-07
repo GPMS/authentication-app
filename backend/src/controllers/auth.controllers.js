021cf3729e3a1cb9194c538049709d4e8481e754
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2023-11-07 18:09:35 -0300

    fix: catch errors

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -25,18 +25,28 @@ export async function register(req, res) {
 
 export async function login(req, res) {
   console.info("Login");
-  const { email, password } = req.body;
-  if (!password || !email) {
-    res.sendStatus(400);
-    return;
+  try {
+    const { email, password } = req.body;
+    if (!password || !email) {
+      res.sendStatus(400);
+      return;
+    }
+    const user = await findUserByEmail(email);
+    if (!user || !(await verifyPassword(password, user.password))) {
+      console.warn("Invalid email or password");
+      res.sendStatus(403);
+      return;
+    }
+    res.send({
+      accessToken: generateToken({ ...user, password: undefined }),
+    });
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(403).send({
+        message: e.message,
+      });
+    } else {
+      res.status(403);
+    }
   }
-  const user = await findUserByEmail(email);
-  if (!user || !(await verifyPassword(password, user.password))) {
-    console.warn("Invalid email or password");
-    res.sendStatus(403);
-    return;
-  }
-  res.send({
-    accessToken: generateToken({ ...user, password: undefined }),
-  });
 }
