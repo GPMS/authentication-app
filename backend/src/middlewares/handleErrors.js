46de14ec6ce7f933f5336d4114ec9297bcc595b3
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2024-03-03 13:01:42 -0300

    fix: log server exception when it occurs

--- backend/src/middlewares/handleErrors.js
+++ backend/src/middlewares/handleErrors.js
@@ -11,5 +11,6 @@ export function handleErrors(err, req, res, next) {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
+  console.log(err);
   return res.status(500).send("Something went wrong try again later");
 }
