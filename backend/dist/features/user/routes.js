730586e345aaa8ee195bf49c00d361cf1d739f7e
Gabriel Portela Macedo Souza <gportelams@gmail.com>
2024-04-08 20:40:23 -0300

    fix: inject missing repository

--- backend/dist/features/user/routes.js
+++ backend/dist/features/user/routes.js
@@ -5,9 +5,10 @@ const express_1 = require("express");
 const verifyToken_1 = require("../../middlewares/verifyToken");
 const controller_1 = require("./controller");
 const service_1 = require("./service");
+const userRepositoryMongoose_1 = require("../../repositories/userRepositoryMongoose");
 exports.userRouter = (0, express_1.Router)();
 function factory() {
-    return new controller_1.UserController(new service_1.UserService());
+    return new controller_1.UserController(new service_1.UserService(new userRepositoryMongoose_1.UserRepositoryMongoose()));
 }
 exports.userRouter.get("/", verifyToken_1.verifyToken, (req, res, next) => {
     factory().getUserInfo(req, res, next);
--- backend/src/features/user/routes.ts
+++ backend/src/features/user/routes.ts
@@ -3,11 +3,12 @@ import { Router } from "express";
 import { verifyToken } from "../../middlewares/verifyToken";
 import { UserController } from "./controller";
 import { UserService } from "./service";
+import { UserRepositoryMongoose } from "../../repositories/userRepositoryMongoose";
 
 export const userRouter = Router();
 
 function factory() {
-  return new UserController(new UserService());
+  return new UserController(new UserService(new UserRepositoryMongoose()));
 }
 
 userRouter.get("/", verifyToken, (req, res, next) => {
